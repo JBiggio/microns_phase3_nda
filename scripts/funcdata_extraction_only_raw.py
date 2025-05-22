98cef2b28d22e9628d8c9f3847e42296b563e638
Jacopo Biggiogera <83726425+JBiggio@users.noreply.github.com>
2025-05-22 21:36:44 +0000

    fixed index error on pd concat

--- scripts/funcdata_extraction_only_raw.py
+++ scripts/funcdata_extraction_only_raw.py
@@ -7,7 +7,7 @@ from tqdm.auto import tqdm
 from scipy.optimize import curve_fit
 #from sklearn.metrics import r2_score
 from ccmodels.preprocessing.connectomics import client_version, subset_v1l234
-from ccmodels.preprocessing.selectivity import orientation_extractor, von_mises, von_mises_single, is_selective, cell_area_identifiers, fpd_assignment, identify_multiscan
+from ccmodels.preprocessing.selectivity import orientation_extractor_v2, von_mises, von_mises_single, is_selective, cell_area_identifiers, fpd_assignment, identify_multiscan
 
 
 #Identify desired neurons, in this case v1 neurons from L234
@@ -24,8 +24,8 @@ v1l234_multiscan = v1l234_neur[v1l234_neur['pt_root_id'].isin(multiscan_ids)]
 session_scan_pairs = [(8,7), (4, 7), (5, 3), (5, 6),(5, 7),(6, 2),(6, 4),(6, 6),(6, 7),(7, 3),(7, 4),(7, 5),(8, 5), (9, 3), (9, 4), (9, 6)]
 
 #Check if storage folders for temporary data exist, if not make them
-if os.path.isdir('../data/in_processing/orientation_fits') != True:
-    os.makedirs('../data/in_processing/orientation_fits')
+#if os.path.isdir('../data/in_processing/orientation_fits') != True:
+#    os.makedirs('../data/in_processing/orientation_fits')
 
 
 print('Starting Extraction of Single Scan...')
@@ -42,10 +42,10 @@ for pair in tqdm(session_scan_pairs, desc = 'Session and Scan Loop'):
     #loop through cells
     for i in tqdm(range(sub.shape[0]), desc = f'Extracting neurons of session: {pair[0]}, scan: {pair[1]}' ):
         unit_key = {'session':sub.iloc[i, 2], 'scan_idx':sub.iloc[i, 3], 'unit_id':sub.iloc[i, 4]}
-        df = orientation_extractor(unit_key, fpd)
+        df = orientation_extractor_v2(unit_key, fpd)
         data.append(df)
     #Save the data
-    data_df =  pd.concat(data, axis = 1)
+    data_df =  pd.concat(data, axis = 0)
     data_df.to_pickle(f'./data/in_processing/activities/activities_{pair[0]}_{pair[1]}.pkl')
         
     #Clean RAM
@@ -58,10 +58,6 @@ print('Starting extraction Multiscan...')
 #Container with saved data
 data = []
             
-#Von Mises
-columns = ['root_id', 'session', 'scan_idx','cell_id', 'pvalue','tuning_type', 
-        'r_squared_diff', 'mean_r_sqrd', 'A', 'phi', 'k', 'activity', 'orientations']
-
 for mult_id in tqdm(multiscan_ids, desc = f'Extracting multiscan neurons data') :
     subset_multiscan = v1l234_multiscan[v1l234_multiscan['pt_root_id'] == mult_id]
     scan_acts = []
@@ -69,19 +65,19 @@ for mult_id in tqdm(multiscan_ids, desc = f'Extracting multiscan neurons data') 
     for row in range(subset_multiscan.shape[0]):
         unit_key = {'session':subset_multiscan['session'].values[row], 'scan_idx':subset_multiscan['scan_idx'].values[row], 'unit_id':subset_multiscan['unit_id'].values[row]}
         fpd = fpd_assignment(subset_multiscan['session'].values[row], subset_multiscan['scan_idx'].values[row])
-        df = orientation_extractor(unit_key, fpd)
+        df = orientation_extractor_v2(unit_key, fpd)
         scan_acts.append(df)
     
-    all_acts = pd.concat(scan_acts, axis = 1)
+    all_acts = pd.concat(scan_acts, axis = 0)
     data.append(all_acts)
 
     
 #Save the data
-data_df =  pd.concat(data, axis = 1)
-data_df.to_pickle(f'./data/in_processing/activities/activities_{pair[0]}_{pair[1]}.pkl')
+data_df =  pd.concat(data, axis = 0)
+data_df.to_pickle(f'./data/in_processing/activities/activities_multiscan.pkl')
 #NOTE: Session, scan_idx and unit_id of these neurons refer to the values in the last row of the subset_multiscan df above
 #Clean RAM
-del sub, unit_key, df
+del unit_key, df
 gc.collect()   
 
 print('Extraction multi scan finished, saving data')
